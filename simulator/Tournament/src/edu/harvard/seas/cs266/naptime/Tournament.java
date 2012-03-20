ae234148322542d8165beb8bc28ae7577021b505
Nicolas Ward <ultranurd@gmail.com>
2012-03-20 05:19:11 +0000

    initialize the tournament field with a bunch of randomly distributed inactive food

--- simulator/Tournament/src/edu/harvard/seas/cs266/naptime/Tournament.java
+++ simulator/Tournament/src/edu/harvard/seas/cs266/naptime/Tournament.java
@@ -7,6 +7,8 @@
 package edu.harvard.seas.cs266.naptime;
 
 import sim.engine.SimState;
+import sim.field.continuous.Continuous2D;
+import sim.util.Double2D;
 
 /**
  * Implements a simulation of the robot foraging field.
@@ -16,12 +18,41 @@ import sim.engine.SimState;
 @SuppressWarnings("serial")
 public class Tournament extends SimState {
 	/**
-	 * Initializes the foraging field.
+	 * Representation of 2-D space where robots will forage for food.
+	 */
+	public Continuous2D field = new Continuous2D(1.0, 220, 150);
+	
+	/**
+	 * Initial count of food particles in the field (may get parameterized).
+	 */
+	public int nTreats = 20;
+	
+	/**
+	 * Creates the simulation.
 	 * 
 	 * @param seed Seed for the simulation's RNG.
 	 */
 	public Tournament(long seed) {
 		super(seed);
+	}
+	
+	/**
+	 * Implements simulation initialization
+	 */
+	public void start() {
+		// Start the simulation
+		super.start();
+		
+		// Clear the field of food and robots
+		field.clear();
+		
+		// Add some randomly distributed food to the field
+		for (int t = 0; t < nTreats; t++) {
+			Treat treat = new Treat();
+			field.setObjectLocation(treat,
+									new Double2D(field.getWidth()*0.5 + random.nextDouble() - 0.5,
+												 field.getHeight()*0.5 + random.nextDouble() - 0.5));
+		}
 	}
 
 	/**
---
+++ simulator/Tournament/src/edu/harvard/seas/cs266/naptime/Treat.java
@@ -0,0 +1,16 @@
+/**
+ * @file Treat.java
+ * @author nward@fas.harvard.edu
+ * @date 2012.03.17
+ */
+
+package edu.harvard.seas.cs266.naptime;
+
+/**
+ * Represents a single item of food on the tournament field.
+ * 
+ * @author nward@fas.harvard.edu
+ */
+public class Treat {
+
+}
