18f45d3ae2ec876a5d60443c6256297520a247e0
Nicolas Ward <ultranurd@gmail.com>
2012-03-22 04:34:20 +0000

    implement realistic differential steering; robots now turn and move to closest objective, sensing not yet realistic

--- simulator/Tournament/src/edu/harvard/seas/cs266/naptime/Robot.java
+++ simulator/Tournament/src/edu/harvard/seas/cs266/naptime/Robot.java
@@ -18,29 +18,43 @@ public class Robot implements Steppable, Oriented2D {
 	 * The current facing of the robot. Its front is where the camera and kicker are.
 	 */
 	private double orientation = 0.0;
+	
+	/**
+	 * The speed (in units per step) of the robot's left motor.
+	 */
+	private double leftSpeed = 0.0;
+	
+	/**
+	 * The speed (in units per step) of the robot's right motor.
+	 */
+	private double rightSpeed = 0.0;
 
+	/**
+	 * Increments the simulated state of this robot one time step.
+	 * 
+	 * @param state The current Tournament simulation.
+	 */
 	@Override
 	public void step(SimState state) {
 		// Get the current simulation
 		Tournament tourney = (Tournament) state;
 		
-		// Determine where we are and where to go
-		Double2D current = tourney.field.getObjectLocation(this);
-		Double2D objective = senseNextObjective(current, tourney.field);
+		// Determine where we are and how to move
+		sense(tourney.field);
 		
-		// Move in that direction, slowing as we approach
-		double distanceToObjective = current.distance(objective);
-		if (distanceToObjective < (Robot.robotSize + Treat.treatSize)/2.0)
-			return;
-		double speed = 1.0;
-		Double2D destination = objective.subtract(current);
-		destination = destination.normalize();
-		destination = destination.resize(speed);
-		destination = destination.add(current);
-		tourney.field.setObjectLocation(this, destination);
+		// Run the "motors" at their current speed settings
+		move(tourney.field);
 	}
-
-	private Double2D senseNextObjective(Double2D current, Continuous2D field) {
+	
+	/**
+	 * View the current Tournament field and determine motor speeds.
+	 * 
+	 * @param field The current Tournament field containing this robot.
+	 */
+	private void sense(Continuous2D field) {
+		// Get the robot's current location
+		Double2D current = field.getObjectLocation(this);
+		
 		// Find the nearest food
 		Bag neighbors = field.getNearestNeighbors(current, 3, false, false, true, null);
 		double minDistance = 2.0*Math.sqrt(Math.pow(field.getWidth(), 2.0) + Math.pow(field.getHeight(), 2.0));
@@ -55,11 +69,72 @@ public class Robot implements Steppable, Oriented2D {
 				}
 			}
 		}
-		if (objective == null)
-			return current;
-		return objective;
+		if (objective == null) {
+			setSpeed(0.0, 0.0);
+			return;
+		}
+		
+		// Determine the distance to the objective
+		double distance = current.distance(objective);
+		
+		// Check if we're within epsilon of the objective
+		if (distance < 0.001) {
+			setSpeed(0.0, 0.0);
+			return;
+		}
+		
+		// Determine the angle to the objective
+		double baseSpeed = 0.1;
+		double objectiveAngle = objective.subtract(current).angle() - orientation;
+		if (objectiveAngle < -baseSpeed/robotSize)
+			// Turn right
+			setSpeed(baseSpeed, -baseSpeed);
+		else if (objectiveAngle > baseSpeed/robotSize)
+			// Turn left
+			setSpeed(-baseSpeed, baseSpeed);
+		else if (distance > 2*baseSpeed)
+			// Move forward at full speed
+			setSpeed(2*baseSpeed, 2*baseSpeed);
+		else
+			// Move to the objective (last step)
+			setSpeed(distance, distance);
+	}
+	
+	/**
+	 * Uses the current speed to adjust the robot's position and orientation.
+	 */
+	private void move(Continuous2D field) {
+		// Update the orientation based on relative wheel velocity
+		orientation += (rightSpeed - leftSpeed)/robotSize;
+		
+		// Keep orientation in [-pi,pi] range even though Oriented2D mods correctly
+		if (orientation > Math.PI)
+			orientation -= 2*Math.PI;
+		else if (orientation < -Math.PI)
+			orientation += 2*Math.PI;
+		
+		// Update the position based on midpoint speed and new orientation
+		Double2D currentPosition = field.getObjectLocation(this);
+		double midpointSpeed = (rightSpeed + leftSpeed)/2;
+		Double2D newPosition = new Double2D(currentPosition.x + midpointSpeed*Math.cos(orientation),
+											currentPosition.y + midpointSpeed*Math.sin(orientation));
+		field.setObjectLocation(this, newPosition);
+	}
+	
+	/**
+	 * Mutator to update the current motor speeds of the robot.
+	 * 
+	 * @param left The desired speed of the left motor.
+	 * @param right The desired speed of the right motor.
+	 */
+	protected void setSpeed(double left, double right) {
+		leftSpeed = left;
+		rightSpeed = right;
 	}
 
+	/**
+	 * Accessor for the robot's current facing. Implements the Oriented2D interface.
+	 */
 	@Override
 	public double orientation2D() {
 		return orientation;
