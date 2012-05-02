85d9dca4d9a4a2fa367423b5b5444096b3a167fb
Nicolas Ward <ultranurd@gmail.com>
2012-05-02 14:57:50 +0000

    initial implementation of C conversion from parsed nested list structure

--- steps/convert_to_c.py
+++ steps/convert_to_c.py
@@ -22,6 +22,86 @@ sexpString = pyparsing.Word(pyparsing.alphas)
 sexp = pyparsing.Forward()
 sexp << pyparsing.Group(pyparsing.And([left_paren, pyparsing.OneOrMore(pyparsing.Or([sexpString, integer, double, sexp])), right_paren]))
 
+operator_map = {
+    "lt":"<",
+    "lte":"<=",
+    "gt":">",
+    "gte":">=",
+    "eq":"==",
+    "and":"&&",
+    "or":"||",
+    "not":"!",
+    }
+
+leaf_operators = [
+    "getMidpoint",
+    "getWidth",
+    "getTravel",
+    "getRotations",
+    "drop",
+    "pickUp",
+    ]
+
+def c_lines(sexp_list):
+    """
+    Generator to produce lines of C from a step program.
+    """
+
+    # Check if this is an atom
+    if type(sexp_list) != list:
+        yield sexp_list
+        return
+
+    # Get and check operator name
+    op = sexp_list.pop(0)
+    if op == "step":
+        for child in sexp_list:
+            for line in c_lines(child):
+                yield line
+    elif op == "if":
+        yield "if (%s) {" % "".join(c_lines(sexp_list[0]))
+        lines = list(c_lines(sexp_list[1]))
+        if len(lines) == 1:
+            yield lines[0] + ";"
+        else:
+            for line in lines:
+                yield line
+        if len(sexp_list) == 3:
+            yield "} else {"
+            lines = list(c_lines(sexp_list[2]))
+            if len(lines) == 1:
+                yield lines[0] + ";"
+            else:
+                for line in lines:
+                    yield line
+        yield "}"
+    elif op in ("and", "or"):
+        if sexp_list[0][0] == "if":
+            for child in sexp_list:
+                for line in c_lines(child):
+                    yield line
+        else:
+            children = []
+            for child in sexp_list:
+                children.extend(c_lines(child))
+            yield (" %s " % operator_map[op]).join(children)
+    elif op == "not":
+        yield operator_map[op] + "".join(c_lines(sexp_list[0]))
+    elif op in ("lt", "lte", "gt", "gte", "eq"):
+        yield "%s %s %s" % ("".join(map(str, c_lines(sexp_list[0]))), operator_map[op], "".join(map(str, c_lines(sexp_list[1]))))
+    elif op == "inState":
+        yield "inState(\"%s\")" % sexp_list[0]
+    elif op == "setState":
+        yield "setState(\"%s\")" % sexp_list[0]
+    elif op == "setSpeed":
+        yield "setSpeed(%f, %f)" % tuple(sexp_list)
+    elif op == "getRange":
+        yield "getRange(%d)" % sexp_list[0]
+    elif op in leaf_operators:
+        yield "%s()" % op
+    else:
+        yield str(sexp_list)
+
 def convert(input_file, output_file):
     """
     Reads the specified input step program S-expression
@@ -34,8 +114,8 @@ def convert(input_file, output_file):
 
     # Dump
     output_f = open(output_file, "w")
-    print parsed
-    output_f.write(str(parsed.asList()))
+    for line in c_lines(parsed.asList()[0]):
+        output_f.write(line + "\n")
     output_f.close()
 
 def main():
